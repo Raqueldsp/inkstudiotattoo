135b43f45363643f19802ab89ec7395efa4b9a37
Raquel <raquelsilvapa@gmail.com>
2023-08-25 12:03:35 -0300

    Criação do ambiente de desenvolvimento

---
+++ src/test/java/com/inkstudio/inkstudiotattoo/InkstudiotattooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inkstudio.inkstudiotattoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InkstudiotattooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
